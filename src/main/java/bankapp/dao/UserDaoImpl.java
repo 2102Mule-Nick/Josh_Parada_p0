5f78b9202fb280472e5909be12ef1cba6545a201
JoshParada <paradajosh@yahoo.com>
2021-03-02 16:55:19 -0600

    added UserDaoImpl

---
+++ src/main/java/bankapp/dao/UserDaoImpl.java
@@ -0,0 +1,77 @@
+package bankapp.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import bankapp.exception.UserNameTaken;
+import bankapp.exception.UserNotFound;
+import bankapp.pojo.User;
+
+public class UserDaoImpl implements UserDao{
+	
+	public static List<User> userList;
+
+	@Override
+	public void createUser(User user) throws UserNameTaken {
+		
+		if (user.getUsername() != null && user.getPassword() != null) {
+			
+			Iterator<User> iter = userList.iterator();
+			
+			while (iter.hasNext()) {
+				if (iter.next().getUsername().equals(user.getUsername())) {
+					throw new UserNameTaken("This username is taken");
+				}
+			}
+			
+			//If username not taken, add user
+			userList.add(user);
+			
+		}
+		
+	}
+
+	@Override
+	public User getUserByUsername(String username) throws UserNotFound {
+
+		Iterator<User> iter = userList.iterator();
+		
+		while (iter.hasNext()) {
+			
+			User existingUser = iter.next();
+			if (existingUser.getUsername().equalsIgnoreCase(username)) {
+				return existingUser;
+			}
+		}
+		
+		throw new UserNotFound();
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public UserDaoImpl() {
+		super();
+		userList = new ArrayList<>();
+		userList.add(new User("keyur", "iamhappy"));
+		userList.add(new User("justbrian", "hunter12"));
+		userList.add(new User("gael", "g22"));
+	}
+	
+}
